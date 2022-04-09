c5831363344258d23559d87ed4c567d46a571514
Q100SZ71 <q100sz71@op.pl>
2022-04-09 13:41:18 +0200

    zmiana powiadomienia

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 9000;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Blanty są SUPER!');
+  res.end('Testowanie jest super SUPER!');
 }
 
 const server = http.createServer(requestListener);
