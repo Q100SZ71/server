35cb7f4da07f468341d5541cbd52bcabf7d723cd
Q100SZ71 <q100sz71@op.pl>
2022-04-09 13:36:31 +0200

    dodanie pliku servera

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const http = require('http');
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 9000;
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end('Testowanie jest super SUPER!');
+  res.end('Blanty są SUPER!');
 }
 
 const server = http.createServer(requestListener);
